7f6609523ee332fa0d3b1f22a48b241f69e22ccf
Adarshjain3011 <adarshjain3011@gamil.com>
2025-06-25 13:38:26 +0530

    minor changes done

--- components/team-management/EditableDropdownCell.jsx
+++ components/team-management/EditableDropdownCell.jsx
@@ -2,18 +2,36 @@
 
 import { user_role } from "@/lib/data";
 import { useState } from "react";
+import { updateMembersData } from "@/lib/api";
+import toast from "react-hot-toast";
+import { handleAxiosError } from "@/lib/handleAxiosError";
 
 const EditableDropdownCell = ({ row, columnId, value, handleEdit }) => {
   const [selectedRole, setSelectedRole] = useState(value);
 
   const handleCommit = async (row, columnId, newRole) => {
+
+    console.log("row ,columnId,newRole : ", row.original._id, columnId, newRole);
+
+    const prepareData = {
+
+      role:newRole,
+      userId:row.original._id,
+
+    }
+
     try {
-      const response = await updateMemberRole(row.original._id, newRole);
-      if (response.success) {
+      const response = await updateMembersData(prepareData);
+
         handleEdit(row.index, columnId, newRole);
-      }
+
+        toast.success("user role update successfully ");
+
     } catch (error) {
       console.error("Error updating role:", error);
+
+      handleAxiosError(error);
+
     }
   };
 
